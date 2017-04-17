55070171fb09aed110153b29ce47aaa5284a9e23
qiunet <qiunet@163.com>
2017-04-17 20:08:59 +0800

    可以终止 线程池

--- QiunetDatas/src/main/java/org/qiunet/data/async/AsyncJobSupport.java
+++ QiunetDatas/src/main/java/org/qiunet/data/async/AsyncJobSupport.java
@@ -2,6 +2,10 @@ package org.qiunet.data.async;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 异步更新的公用类
@@ -9,13 +13,14 @@ import java.util.List;
  *         Created on 17/2/11 08:04.
  */
 public class AsyncJobSupport {
-	
+	private ExecutorService executor = new ThreadPoolExecutor(10, 50, 10, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>(30));
+
 	private volatile static AsyncJobSupport instance;
-	
+
 	private AsyncJobSupport() {
 		instance = this;
 	}
-	
+
 	public static AsyncJobSupport getInstance() {
 		if (instance == null) {
 			synchronized (AsyncJobSupport.class) {
@@ -27,19 +32,36 @@ public class AsyncJobSupport {
 		}
 		return instance;
 	}
-	
+
 	private List<AsyncNode> nodes = new ArrayList<>();
-	
+
 	public void addNode(AsyncNode node) {
 		this.nodes.add(node);
 	}
-	
+
+	/***
+	 * 停止线程池
+	 */
+	public void shutdown(){
+		executor.shutdown();
+	}
+
 	/***
 	 * 异步更新到db
 	 */
 	public void asyncToDb(){
-		for (AsyncNode node : nodes) {
-			node.updateRedisDataToDatabase();
+		for (final AsyncNode node : nodes) {
+			executor.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						// 必须try catch 否则导致线程停止
+						node.updateRedisDataToDatabase();
+					}catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
 		}
 	}
 }
