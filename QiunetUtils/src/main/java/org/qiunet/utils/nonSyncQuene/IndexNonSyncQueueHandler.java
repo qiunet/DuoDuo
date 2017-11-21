90b984cb0965f3151f841b606a2a6faefb5c8e22
qiunet <qiunet@163.com>
2017-11-21 17:12:26 +0800

    thread name 明确

--- QiunetUtils/src/main/java/org/qiunet/utils/nonSyncQuene/IndexNonSyncQueueHandler.java
+++ QiunetUtils/src/main/java/org/qiunet/utils/nonSyncQuene/IndexNonSyncQueueHandler.java
@@ -1,30 +1,30 @@
 package org.qiunet.utils.nonSyncQuene;
 
 /**
- * 索引分配使用的哪个 NonSyncQueueHandler 
+ * 索引分配使用的哪个 NonSyncQueueHandler
  * @author qiunet
  *         Created on 17/3/14 11:22.
  */
 public class IndexNonSyncQueueHandler<Element extends QueueElement> {
-	
-	private static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors(); 
-	
+
+	private static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors();
+
 	private NonSyncQueueHandler[] arrays;
 	/**本机的线程数量*/
 	private int currThreadCount;
-	
+
 	public IndexNonSyncQueueHandler (boolean daemon ) {
 		this(THREAD_COUNT, daemon);
 	}
 	public IndexNonSyncQueueHandler (int threadCount , boolean daemon ) {
-		
+
 		if (threadCount < 1) throw new Error("Thread count can not less than 1 !");
-		
+
 		this.currThreadCount = threadCount;
-		
+
 		this.arrays = new NonSyncQueueHandler[threadCount];
 		for (int i = 0 ; i < threadCount ; i++) {
-			this.arrays[i] = NonSyncQueueHandler.create("Handler["+i+"]: ", daemon);
+			this.arrays[i] = NonSyncQueueHandler.create("IndexNonSyncQueueHandler["+i+"]: ", daemon);
 		}
 	}
 
