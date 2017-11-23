cd1be76229afd39e5cf22a905bbbf591eb2d8594
qiunet <qiunet@163.com>
2017-11-23 09:54:12 +0800

    把扫描request 单弄出来

---
+++ FlashHandler/src/test/java/org/qiunet/flash/handler/handler/mapping/RequestHandlerScanner.java
@@ -0,0 +1,34 @@
+package org.qiunet.flash.handler.handler.mapping;
+
+import org.qiunet.flash.handler.common.annotation.support.RequestScannerHandler;
+import org.qiunet.flash.handler.gamecfg.GiftCfg;
+import org.qiunet.utils.classScanner.ScannerAllClassFile;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+/**
+ * 自行加载requestHandler
+ * Created by qiunet.
+ * 17/11/22
+ */
+
+public abstract class RequestHandlerScanner {
+
+	public RequestHandlerScanner(){
+		ScannerAllClassFile scannerAllClassFile = new ScannerAllClassFile();
+
+		try {
+			scannerAllClassFile.scannerJarFile(GiftCfg.class.getResource("").toURI().toURL());
+			scannerAllClassFile.addScannerHandler(new RequestScannerHandler());
+
+			scannerAllClassFile.scanner();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- FlashHandler/src/test/java/org/qiunet/flash/handler/handler/mapping/TestMapping.java
+++ FlashHandler/src/test/java/org/qiunet/flash/handler/handler/mapping/TestMapping.java
@@ -2,13 +2,10 @@ package org.qiunet.flash.handler.handler.mapping;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.qiunet.flash.handler.common.annotation.support.RequestScannerHandler;
 import org.qiunet.flash.handler.common.enums.DataType;
-import org.qiunet.flash.handler.gamecfg.GiftCfg;
 import org.qiunet.flash.handler.handler.IHandler;
 import org.qiunet.flash.handler.handler.http.LoginHandler;
 import org.qiunet.flash.handler.handler.http.LoginProtobufHandler;
-import org.qiunet.utils.classScanner.ScannerAllClassFile;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
@@ -17,16 +14,9 @@ import java.net.URISyntaxException;
  * Created by qiunet.
  * 17/11/22
  */
-public class TestMapping {
+public class TestMapping extends RequestHandlerScanner {
 	@Test
 	public void scannerRequestHandler() throws URISyntaxException, MalformedURLException, ClassNotFoundException {
-		ScannerAllClassFile scannerAllClassFile = new ScannerAllClassFile();
-
-		scannerAllClassFile.scannerJarFile(GiftCfg.class.getResource("").toURI().toURL());
-		scannerAllClassFile.addScannerHandler(new RequestScannerHandler());
-
-		scannerAllClassFile.scanner();
-
 		IHandler handler = RequestHandlerMapping.getInstance().getHandler(1000);
 		Assert.assertEquals(handler.getDataType() , DataType.STRING);
 		Assert.assertTrue(handler instanceof LoginHandler);
