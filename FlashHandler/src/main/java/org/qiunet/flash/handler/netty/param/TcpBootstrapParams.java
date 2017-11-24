d11f168c380636e57d13d10e78cd427319173b2f
qiunet <qiunet@163.com>
2017-11-24 10:10:27 +0800

    对tcp接收的消息长度进行了限制

--- FlashHandler/src/main/java/org/qiunet/flash/handler/netty/param/TcpBootstrapParams.java
+++ FlashHandler/src/main/java/org/qiunet/flash/handler/netty/param/TcpBootstrapParams.java
@@ -7,7 +7,13 @@ package org.qiunet.flash.handler.netty.param;
  * 17/7/19
  */
 public final class TcpBootstrapParams extends AbstractBootstrapParam {
+	private int maxReceivedLength;
 	private TcpBootstrapParams(){}
+
+	public int getMaxReceivedLength() {
+		return maxReceivedLength;
+	}
+
 	/***
 	 * 得到
 	 * @return
@@ -20,7 +26,15 @@ public final class TcpBootstrapParams extends AbstractBootstrapParam {
 	 * 使用build模式 set和 get 分离. 以后有有顺序的构造时候也可以不动
 	 */
 	public static class Builder extends SuperBuilder<TcpBootstrapParams, Builder> {
+		// 最大上行1M的长度
+		private int maxReceivedLength = 1024 * 1024;
+
 		 private Builder(){}
+
+		public Builder setMaxReceivedLength(int maxReceivedLength) {
+			this.maxReceivedLength = maxReceivedLength;
+			return this;
+		}
 
 		@Override
 		protected TcpBootstrapParams newParams() {
@@ -29,7 +43,7 @@ public final class TcpBootstrapParams extends AbstractBootstrapParam {
 
 		@Override
 		protected void buildInner(TcpBootstrapParams tcpBootstrapParams) {
-
+			tcpBootstrapParams.maxReceivedLength = this.maxReceivedLength;
 		}
 	}
 }
