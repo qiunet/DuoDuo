9f5649a69c94223fbd6030aa80039e9f01009d9a
qiunet <qiunet@163.com>
2017-11-27 15:51:52 +0800

    让http初始化完成再测试.

--- FlashHandler/src/test/java/org/qiunet/flash/handler/bootstrap/HttpBootStrap.java
+++ FlashHandler/src/test/java/org/qiunet/flash/handler/bootstrap/HttpBootStrap.java
@@ -9,14 +9,18 @@ import org.qiunet.flash.handler.netty.server.BootstrapServer;
 import org.qiunet.flash.handler.netty.server.hook.Hook;
 import org.qiunet.flash.handler.netty.server.param.HttpBootstrapParams;
 
+import java.util.concurrent.locks.LockSupport;
+
 /**
  * Created by qiunet.
  * 17/11/25
  */
 public class HttpBootStrap extends RequestHandlerScanner {
 	private static Hook hook = new MyHook();
+	private static Thread currThread;
 	@BeforeClass
 	public static void init() {
+		currThread = Thread.currentThread();
 		Thread thread = new Thread(new Runnable() {
 			@Override
 			public void run() {
@@ -24,10 +28,13 @@ public class HttpBootStrap extends RequestHandlerScanner {
 						.setInterceptor(new DefaultHttpInterceptor())
 						.setPort(8080)
 						.build();
-				BootstrapServer.createBootstrap(hook).httpListener(httpParams).await();
+				BootstrapServer server = BootstrapServer.createBootstrap(hook).httpListener(httpParams);
+				LockSupport.unpark(currThread);
+				server.await();
 			}
 		});
 		thread.start();
+		LockSupport.park();
 	}
 
 	@AfterClass
