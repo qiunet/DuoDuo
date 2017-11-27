8480e2a9ebe8aa94dd8fae0cd379b0c6ef631e06
qiunet <qiunet@163.com>
2017-11-27 15:30:28 +0800

    大量请求的泄漏测试

---
+++ FlashHandler/src/test/java/org/qiunet/flash/handler/bootstrap/MuchTcpRequest.java
@@ -0,0 +1,79 @@
+package org.qiunet.flash.handler.bootstrap;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.qiunet.flash.handler.bootstrap.error.DefaultErrorMessage;
+import org.qiunet.flash.handler.bootstrap.hook.MyHook;
+import org.qiunet.flash.handler.context.header.MessageContent;
+import org.qiunet.flash.handler.handler.mapping.RequestHandlerScanner;
+import org.qiunet.flash.handler.interceptor.DefaultTcpInterceptor;
+import org.qiunet.flash.handler.netty.client.tcp.ITcpResponseTrigger;
+import org.qiunet.flash.handler.netty.client.tcp.NettyTcpClient;
+import org.qiunet.flash.handler.netty.server.BootstrapServer;
+import org.qiunet.flash.handler.netty.server.hook.Hook;
+import org.qiunet.flash.handler.netty.server.param.TcpBootstrapParams;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * Created by qiunet.
+ * 17/11/27
+ */
+public abstract class MuchTcpRequest extends RequestHandlerScanner implements ITcpResponseTrigger {
+	protected static String host = "localhost";
+	protected static int port = 8889;
+	protected static Hook hook = new MyHook();
+	protected NettyTcpClient tcpClient;
+	private static Thread currThread;
+	@BeforeClass
+	public static void init(){
+		currThread = Thread.currentThread();
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				TcpBootstrapParams tcpParams = TcpBootstrapParams.custom()
+						.setInterceptor(new DefaultTcpInterceptor())
+						.setErrorMessage(new DefaultErrorMessage())
+						.setPort(port)
+						.build();
+				BootstrapServer server = BootstrapServer.createBootstrap(hook).tcpListener(tcpParams);
+				LockSupport.unpark(currThread);
+				server.await();
+			}
+		});
+		thread.start();
+		LockSupport.park();
+	}
+	@Before
+	public void connect(){
+		currThread = Thread.currentThread();
+		try {
+			tcpClient = new NettyTcpClient(new InetSocketAddress(InetAddress.getByName(host), port), this);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	@After
+	public void closeConnect(){
+		tcpClient.close();
+	}
+
+	@Override
+	public void response(MessageContent data) {
+		this.responseTcpMessage(data);
+	}
+
+	protected abstract void responseTcpMessage(MessageContent data);
+
+	@AfterClass
+	public static void shutdown() throws InterruptedException {
+		BootstrapServer.sendHookMsg(hook.getHookPort(), hook.getShutdownMsg());
+	}
+}
+
+
---
+++ FlashHandler/src/test/java/org/qiunet/flash/handler/bootstrap/TestMuchTcpRequest.java
@@ -0,0 +1,57 @@
+package org.qiunet.flash.handler.bootstrap;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.junit.Test;
+import org.qiunet.flash.handler.context.header.MessageContent;
+import org.qiunet.flash.handler.handler.proto.LoginProto;
+import org.qiunet.flash.handler.netty.client.tcp.NettyTcpClient;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by qiunet.
+ * 17/11/27
+ */
+public class TestMuchTcpRequest extends MuchTcpRequest {
+	private int requestCount = 1000000;
+	private CountDownLatch latch = new CountDownLatch(requestCount);
+	@Test
+	public void muchRequest() throws InterruptedException {
+		long start = System.currentTimeMillis();
+		final int threadCount = 50;
+		final NettyTcpClient tcpClient = this.tcpClient;
+
+		for (int j = 0; j < threadCount; j++) {
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					int count = requestCount/threadCount;
+					for (int i = 0 ; i < count; i ++) {
+						String text = "test [testTcpProtobuf]: "+i;
+						LoginProto.LoginRequest request = LoginProto.LoginRequest.newBuilder().setTestString(text).build();
+						MessageContent content = new MessageContent(1004, request.toByteArray());
+						tcpClient.sendTcpMessage(content);
+					}
+				}
+			}).start();
+		}
+
+		latch.await();
+		long end = System.currentTimeMillis();
+		Thread.sleep(1000);
+
+		System.out.println("All Time is:["+(end - start)+"]ms");
+		Thread.sleep(100000);
+	}
+	@Override
+	protected void responseTcpMessage(MessageContent data) {
+		LoginProto.LoginResponse response = null;
+		try {
+			response = LoginProto.LoginResponse.parseFrom(data.bytes());
+		} catch (InvalidProtocolBufferException e) {
+			e.printStackTrace();
+		}
+		System.out.println(response.getTestString());
+		latch.countDown();
+	}
+}
