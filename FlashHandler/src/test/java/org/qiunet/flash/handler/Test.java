c3dfdd5eeaf3114a4793b09ea5e65aec32a65778
qiunet <qiunet@163.com>
2017-12-15 17:27:05 +0800

    Test 用来debug 跟踪用

---
+++ FlashHandler/src/test/java/org/qiunet/flash/handler/Test.java
@@ -0,0 +1,27 @@
+package org.qiunet.flash.handler;
+
+import org.qiunet.flash.handler.bootstrap.error.DefaultErrorMessage;
+import org.qiunet.flash.handler.bootstrap.hook.MyHook;
+import org.qiunet.flash.handler.interceptor.DefaultTcpInterceptor;
+import org.qiunet.flash.handler.netty.server.BootstrapServer;
+import org.qiunet.flash.handler.netty.server.hook.Hook;
+import org.qiunet.flash.handler.netty.server.param.TcpBootstrapParams;
+
+/**
+ * 用来debug 跟踪内部测试用
+ * Created by qiunet.
+ * 17/12/15
+ */
+public class Test {
+	private static Hook hook = new MyHook();
+	public static void main(String[] args) {
+		BootstrapServer.createBootstrap(hook).tcpListener(
+				TcpBootstrapParams.custom()
+						.setTcpInterceptor(new DefaultTcpInterceptor())
+						.setErrorMessage(new DefaultErrorMessage())
+						.setPort(8888)
+						.setCrc(true)
+					.build()
+		).await();
+	}
+}
