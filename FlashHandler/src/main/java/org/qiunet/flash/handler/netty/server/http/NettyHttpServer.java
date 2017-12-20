a632144fd3c4aea379ef5bb518467c90704ce8e3
qiunet <qiunet@163.com>
2017-12-20 08:33:06 +0800

    http bug 修复

--- FlashHandler/src/main/java/org/qiunet/flash/handler/netty/server/http/NettyHttpServer.java
+++ FlashHandler/src/main/java/org/qiunet/flash/handler/netty/server/http/NettyHttpServer.java
@@ -52,7 +52,7 @@ public class NettyHttpServer implements Runnable {
 
 			bootstrap.channel(NioServerSocketChannel.class);
 			bootstrap.childHandler(new NettyHttpServerInitializer(sslCtx, params));
-
+			bootstrap.option(ChannelOption.SO_REUSEADDR, true);
 			bootstrap.option(ChannelOption.SO_BACKLOG, 2048);
 			this.channelFuture = bootstrap.bind(params.getAddress()).sync();
 			qLogger.error("[NettyHttpServer]  Http server is started by " +
