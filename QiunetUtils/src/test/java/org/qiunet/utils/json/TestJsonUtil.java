ea255d84cc00b5cece8d0208d814c231556670c7
qiunet <qiunet@163.com>
2018-01-09 10:58:40 +0800

    json 测试用例添加一种情况

--- QiunetUtils/src/test/java/org/qiunet/utils/json/TestJsonUtil.java
+++ QiunetUtils/src/test/java/org/qiunet/utils/json/TestJsonUtil.java
@@ -38,5 +38,21 @@ public class TestJsonUtil extends BaseTest{
 		Assert.assertTrue(ret.contains(3));
 		Assert.assertFalse(ret.contains(5));
 	}
+	@Test
+	public void testRefJson(){
+		Map<Integer, Map<String, String>> map = new HashMap<>();
+		Map<String, String> subMap = new HashMap<>();
+		subMap.put("ATK", "aa");
+		subMap.put("DEF", "bb");
+		map.put(1, subMap);
+		map.put(2, subMap);
+		map.put(3, subMap);
 
+		String json = JsonUtil.toJsonString(map);
+		Assert.assertEquals("{1:{\"DEF\":\"bb\",\"ATK\":\"aa\"},2:{\"DEF\":\"bb\",\"ATK\":\"aa\"},3:{\"DEF\":\"bb\",\"ATK\":\"aa\"}}", json);
+		map = JsonUtil.getGeneralObject(json, Map.class);
+		subMap = map.get(2);
+		Assert.assertNotNull(subMap);
+		Assert.assertEquals("bb", subMap.get("DEF"));
+	}
 }
