f93381c2269d06c07d450eadaf2bbc36072b1f44
qiunet <qiunet@163.com>
2018-01-18 10:03:46 +0800

    简化ServerChannelGroup

--- FlashHandler/src/main/java/org/qiunet/flash/handler/netty/server/http/channel/ServerChannelGroup.java
+++ FlashHandler/src/main/java/org/qiunet/flash/handler/netty/server/http/channel/ServerChannelGroup.java
@@ -1,8 +1,5 @@
 package org.qiunet.flash.handler.netty.server.http.channel;
 
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelId;
-import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.ChannelGroupFuture;
 import io.netty.channel.group.ChannelMatcher;
 import io.netty.channel.group.DefaultChannelGroup;
@@ -13,50 +10,16 @@ import io.netty.util.concurrent.GlobalEventExecutor;
  * Created by qiunet.
  * 17/11/30
  */
-public class ServerChannelGroup<Msg> {
-	private final ChannelGroup channelGroup;
+public class ServerChannelGroup<Msg> extends DefaultChannelGroup {
 
 	public ServerChannelGroup(String name) {
-		channelGroup = new DefaultChannelGroup(name, GlobalEventExecutor.INSTANCE);
+		super(name, GlobalEventExecutor.INSTANCE);
 	}
-
-	public void add(Channel channel) {
-		channelGroup.add(channel);
-	}
-
 	public ChannelGroupFuture broadcast(Msg msg) {
-		return channelGroup.writeAndFlush(msg);
+		return this.writeAndFlush(msg);
 	}
 
 	public ChannelGroupFuture broadcast(Msg msg, ChannelMatcher matcher) {
-		return channelGroup.writeAndFlush(msg, matcher);
-	}
-
-	public Channel find(ChannelId channelId) {
-		return channelGroup.find(channelId);
-	}
-
-	public ChannelGroup flush() {
-		return channelGroup.flush();
-	}
-
-	public boolean remove(Channel channel) {
-		return channelGroup.remove(channel);
-	}
-
-	public ChannelGroupFuture disconnect() {
-		return channelGroup.disconnect();
-	}
-
-	public ChannelGroupFuture disconnect(ChannelMatcher matcher) {
-		return channelGroup.disconnect(matcher);
-	}
-
-	public boolean contains(Channel channel) {
-		return channelGroup.contains(channel);
-	}
-
-	public int size() {
-		return channelGroup.size();
+		return this.writeAndFlush(msg, matcher);
 	}
 }
