be99bd984878ba3bd4d1c2d9f8b85c43674ef4a0
qiunet <qiunet@163.com>
2018-01-18 11:14:53 +0800

    ChannelGroup 适应自己的框架.

--- FlashHandler/src/main/java/org/qiunet/flash/handler/netty/server/http/channel/ServerChannelGroup.java
+++ FlashHandler/src/main/java/org/qiunet/flash/handler/netty/server/http/channel/ServerChannelGroup.java
@@ -2,24 +2,86 @@ package org.qiunet.flash.handler.netty.server.http.channel;
 
 import io.netty.channel.group.ChannelGroupFuture;
 import io.netty.channel.group.ChannelMatcher;
+import io.netty.channel.group.ChannelMatchers;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
+import org.qiunet.flash.handler.common.message.MessageContent;
 
 /**
  * 方便广播使用的组
  * Created by qiunet.
  * 17/11/30
  */
-public class ServerChannelGroup<Msg> extends DefaultChannelGroup {
+public abstract class ServerChannelGroup<Msg> extends DefaultChannelGroup {
 
 	public ServerChannelGroup(String name) {
 		super(name, GlobalEventExecutor.INSTANCE);
 	}
-	public ChannelGroupFuture broadcast(Msg msg) {
-		return this.writeAndFlush(msg);
+
+	/***
+	 * 对所有人群发一个消息
+	 * @param protocolId
+	 * @param msg
+	 * @return
+	 */
+	public ChannelGroupFuture broadcast(int protocolId, Msg msg) {
+		return this.broadcast(protocolId, msg, ChannelMatchers.all());
+	}
+	/***
+	 * 群发一个msg
+	 * @param protocolId 协议id
+	 * @param msg 消息
+	 * @param matcher 匹配选择对象
+	 * @return
+	 */
+	public ChannelGroupFuture broadcast(int protocolId, Msg msg, ChannelMatcher matcher) {
+		MessageContent content = new MessageContent(protocolId, bytes(msg));
+		return super.writeAndFlush(content, matcher);
 	}
 
-	public ChannelGroupFuture broadcast(Msg msg, ChannelMatcher matcher) {
-		return this.writeAndFlush(msg, matcher);
+	@Override
+	public ChannelGroupFuture writeAndFlush(Object message) {
+		throw new RuntimeException("Can called this method , cause message not encode!");
 	}
+
+	@Override
+	public ChannelGroupFuture write(Object message) {
+		throw new RuntimeException("Can called this method , cause message not encode!");
+	}
+
+	@Override
+	public ChannelGroupFuture write(Object message, ChannelMatcher matcher) {
+		throw new RuntimeException("Can called this method , cause message not encode!");
+	}
+
+	@Override
+	public ChannelGroupFuture write(Object message, ChannelMatcher matcher, boolean voidPromise) {
+		throw new RuntimeException("Can called this method , cause message not encode!");
+	}
+
+	@Override
+	public ChannelGroupFuture writeAndFlush(Object message, ChannelMatcher matcher) {
+		throw new RuntimeException("Can called this method , cause message not encode!");
+	}
+
+	@Override
+	public ChannelGroupFuture writeAndFlush(Object message, ChannelMatcher matcher, boolean voidPromise) {
+		throw new RuntimeException("Can called this method , cause message not encode!");
+	}
+
+	@Override
+	public ChannelGroupFuture flushAndWrite(Object message) {
+		throw new RuntimeException("Can called this method , cause message not encode!");
+	}
+
+	@Override
+	public ChannelGroupFuture flushAndWrite(Object message, ChannelMatcher matcher) {
+		throw new RuntimeException("Can called this method , cause message not encode!");
+	}
+	/***
+	 * 转换成bytes
+	 * @param msg
+	 * @return
+	 */
+	protected abstract byte[] bytes(Msg msg);
 }
