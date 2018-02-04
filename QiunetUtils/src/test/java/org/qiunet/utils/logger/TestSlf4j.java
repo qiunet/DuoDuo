bea145f7fc232d56d553b5161adb3ccac5716d69
qiunet <qiunet@163.com>
2018-02-04 17:01:33 +0800

    日志的修改

---
+++ QiunetUtils/src/test/java/org/qiunet/utils/logger/TestSlf4j.java
@@ -0,0 +1,17 @@
+package org.qiunet.utils.logger;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by qiunet.
+ * 18/2/4
+ */
+public class TestSlf4j {
+	@Test
+	public void test(){
+		Logger logger = LoggerFactory.getLogger(getClass());
+		logger.info("12345");
+	}
+}
