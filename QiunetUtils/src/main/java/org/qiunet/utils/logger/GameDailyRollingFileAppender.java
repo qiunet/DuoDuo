ed35b7af8724494474cb656996d284f10fc068a8
qiunet <qiunet@163.com>
2018-02-05 12:34:25 +0800

    去掉GameLog 的东西. 以后方便slf4j的使用

--- QiunetUtils/src/main/java/org/qiunet/utils/logger/GameDailyRollingFileAppender.java
+++ QiunetUtils/src/main/java/org/qiunet/utils/logger/GameDailyRollingFileAppender.java
@@ -2,11 +2,6 @@ package org.qiunet.utils.logger;
 
 import org.apache.log4j.DailyRollingFileAppender;
 import org.apache.log4j.Layout;
-import org.apache.log4j.Level;
-import org.apache.log4j.helpers.LogLog;
-import org.apache.log4j.spi.LoggingEvent;
-import org.qiunet.utils.logger.base.GameFileOutPutStream;
-import org.qiunet.utils.logger.base.TheGameLoggerUtil;
 
 import java.io.*;
 
