d210903e1e60d2cf308afafce680a66201b4adc2
qiunet <qiunet@163.com>
2018-05-16 11:51:58 +0800

    压测一下

--- LoggerSender/src/test/java/org/qiunet/logger/sender/test/TestLoggerSender.java
+++ LoggerSender/src/test/java/org/qiunet/logger/sender/test/TestLoggerSender.java
@@ -8,8 +8,10 @@ public class TestLoggerSender {
 	private static final LoggerSender sender = new LoggerSender(RemoteProperties.getInstance().getString("remoteIp"), RemoteProperties.getInstance().getInt("remotePort"),RemoteProperties.getInstance().getShort("gameId"));
 	@Test
 	public void testLogger() throws InterruptedException {
-		sender.sendLog("ItemLog", "qiunet|sss|yyy");
-		sender.sendImportantLog("RechargeLog", "qiunet|sss|yyy");
-		Thread.sleep(5000);
+		for (int i = 0; i < 500; i++) {
+			sender.sendLog("ItemLog", "qiunet|sss|yyy"+i);
+//			sender.sendImportantLog("RechargeLog", "qiunet|sss|yyy"+i);
+		}
+		Thread.sleep(10000);
 	}
 }
