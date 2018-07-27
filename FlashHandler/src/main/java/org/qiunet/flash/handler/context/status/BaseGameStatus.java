802449d86a4c8acf8c84061fe0685c443c066e18
qiunet <qiunet@163.com>
2018-07-27 17:00:32 +0800

    GameStatus 成功为0 很多js的protobuf解析会有问题. 定义为1

--- FlashHandler/src/main/java/org/qiunet/flash/handler/context/status/BaseGameStatus.java
+++ FlashHandler/src/main/java/org/qiunet/flash/handler/context/status/BaseGameStatus.java
@@ -5,16 +5,16 @@ package org.qiunet.flash.handler.context.status;
  * 17/12/9
  */
 public enum  BaseGameStatus implements IGameStatus {
-	EXCEPTION(-1, "服务器出现问题了"),
-	SUCCESS(0, ""),
-	FAST_REQUEST(1, "请求频繁"),
+	SUCCESS(1, "成功"),
 	TOKEN_ERROR(2, "TOKEN错误"),
 	MAINTENANCE(3, "服务器维护中"),
 	BAN(4, "被封号, 禁止登陆了"),
+	FAST_REQUEST(5, "请求频繁"),
 
 	PARAMS_ERROR(10, "参数错误"),
 
 	HANdLER_NOT_FOUND(404, "没有Cmdid对应的RequestHandler"),
+	EXCEPTION(500, "服务器出现问题了"),
 	;
 	private int status;
 	private String desc;
