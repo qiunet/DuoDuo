9ae54325a33d0567161dfefd1d1de67855e08b85
qiunet <qiunet@163.com>
2018-12-11 11:31:36 +0800

    调试Tcp

--- FlashHandler/src/test/java/org/qiunet/flash/handler/Test.java
+++ FlashHandler/src/test/java/org/qiunet/flash/handler/Test.java
@@ -3,10 +3,10 @@ package org.qiunet.flash.handler;
 import org.qiunet.flash.handler.bootstrap.error.DefaultErrorMessage;
 import org.qiunet.flash.handler.bootstrap.hook.MyHook;
 import org.qiunet.flash.handler.context.session.DefaultSessionEvent;
-import org.qiunet.flash.handler.interceptor.DefaultUdpInterceptor;
+import org.qiunet.flash.handler.interceptor.DefaultTcpInterceptor;
 import org.qiunet.flash.handler.netty.server.BootstrapServer;
 import org.qiunet.flash.handler.netty.server.hook.Hook;
-import org.qiunet.flash.handler.netty.server.param.UdpBootstrapParams;
+import org.qiunet.flash.handler.netty.server.param.TcpBootstrapParams;
 
 /**
  * 用来debug 跟踪内部测试用
@@ -16,9 +16,9 @@ import org.qiunet.flash.handler.netty.server.param.UdpBootstrapParams;
 public class Test {
 	private static Hook hook = new MyHook();
 	public static void main(String[] args) {
-		BootstrapServer.createBootstrap(hook).udpListener(
-				UdpBootstrapParams.custom()
-						.setUdpInterceptor(new DefaultUdpInterceptor())
+		BootstrapServer.createBootstrap(hook).tcpListener(
+				TcpBootstrapParams.custom()
+						.setTcpInterceptor(new DefaultTcpInterceptor())
 						.setErrorMessage(new DefaultErrorMessage())
 						.setSessionEvent(new DefaultSessionEvent())
 						.setPort(8888)
