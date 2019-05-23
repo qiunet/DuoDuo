4a737391d6b4fc6a6854bc4333ba9db754b021eb
qiunet <cLAsj8.mU2cgJ&P>
2019-05-23 21:13:23 +0800

    message send

--- LoggerSender/src/main/java/org/qiunet/logger/sender/HandlerMsgQueue.java
+++ LoggerSender/src/main/java/org/qiunet/logger/sender/HandlerMsgQueue.java
@@ -1,34 +1,20 @@
 package org.qiunet.logger.sender;
 
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicLong;
 
-class HandlerMsgQueue implements Runnable {
+class HandlerMsgQueue {
 	AtomicLong atomicLong = new AtomicLong();
-	/*队列*/
-	private final LinkedBlockingQueue<IMessage> queue = new LinkedBlockingQueue<>();
-
-	boolean RUNNING = true;
+	private ExecutorService executorService = Executors.newFixedThreadPool(2);
 	/***
 	 * 添加一个元素
 	 * @param message
 	 */
 	void add(IMessage message) {
-		this.queue.add(message);
-	}
-
-	@Override
-	public void run() {
-		while(RUNNING){
-			try {
-				IMessage message = queue.take();
-				message.send();
-
-				Thread.sleep(1);
-				atomicLong.incrementAndGet();
-			}catch (Exception e){
-				//出现异常, 不捕获. 会导致线程停止了
-			}
-		}
+		this.executorService.submit(() -> {
+			atomicLong.incrementAndGet();
+			message.send();
+		});
 	}
 }
