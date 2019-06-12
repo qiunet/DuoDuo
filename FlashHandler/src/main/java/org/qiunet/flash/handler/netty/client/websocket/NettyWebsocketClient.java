5a2cca6b5ba1267bc32307640ceae2d477a55d54
qiunet <cLAsj8.mU2cgJ&P>
2019-06-12 11:45:39 +0800

    webSocket Client 使用pipeline 解决数据转换的问题.

--- FlashHandler/src/main/java/org/qiunet/flash/handler/netty/client/websocket/NettyWebsocketClient.java
+++ FlashHandler/src/main/java/org/qiunet/flash/handler/netty/client/websocket/NettyWebsocketClient.java
@@ -10,20 +10,17 @@ import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpClientCodec;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.websocketx.*;
-import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketClientCompressionHandler;
 import org.qiunet.flash.handler.common.message.MessageContent;
-import org.qiunet.flash.handler.context.header.IProtocolHeader;
-import org.qiunet.flash.handler.context.header.DefaultProtocolHeader;
 import org.qiunet.flash.handler.netty.client.ILongConnClient;
 import org.qiunet.flash.handler.netty.client.param.WebSocketClientParams;
 import org.qiunet.flash.handler.netty.client.trigger.ILongConnResponseTrigger;
+import org.qiunet.flash.handler.netty.coder.WebSocketDecoder;
+import org.qiunet.flash.handler.netty.coder.WebSocketEncoder;
 import org.qiunet.flash.handler.netty.server.constants.ServerConstants;
-import org.qiunet.flash.handler.util.ChannelUtil;
+import org.qiunet.flash.handler.netty.server.http.handler.WebSocketFrameToByteBufHandler;
 import org.qiunet.utils.logger.LoggerType;
 import org.qiunet.utils.asyncQuene.factory.DefaultThreadFactory;
 import org.slf4j.Logger;
-
-import java.net.URI;
 
 /**
  * Created by qiunet.
@@ -41,24 +38,19 @@ public class NettyWebsocketClient implements ILongConnClient {
 		Bootstrap bootstrap = new Bootstrap();
 		bootstrap.group(group);
 
-		NettyClientHandler handler = new NettyClientHandler(
-				WebSocketClientHandshakerFactory.newHandshaker(
-						params.getURI(), WebSocketVersion.V13, null, true, new DefaultHttpHeaders()));
-
 		bootstrap.channel(NioSocketChannel.class);
 		bootstrap.option(ChannelOption.TCP_NODELAY,true);
-		bootstrap.handler(new NettyWebsocketClient.NettyClientInitializer(handler, params));
+		bootstrap.handler(new NettyWebsocketClient.NettyClientInitializer(params));
 		try {
-			bootstrap.connect(params.getAddress()).sync();
-			handler.handshakeFuture().sync();
+			ChannelFuture future = bootstrap.connect(params.getAddress()).sync();
+			((NettyClientHandler) future.channel().pipeline().get("NettyClientHandler")).handshakeFuture().sync();
 		} catch (Exception e) {
 			logger.error("Exception", e);
 		}
 	}
 	@Override
 	public void sendMessage(MessageContent content){
-		BinaryWebSocketFrame frame = new BinaryWebSocketFrame(ChannelUtil.messageContentToByteBuf(content, channelHandlerContext.channel()));
-		channelHandlerContext.channel().writeAndFlush(frame);
+		channelHandlerContext.channel().writeAndFlush(content);
 	}
 
 	@Override
@@ -73,34 +65,34 @@ public class NettyWebsocketClient implements ILongConnClient {
 
 
 	private class NettyClientInitializer extends ChannelInitializer<SocketChannel> {
-		private NettyClientHandler handler;
 		private WebSocketClientParams params;
-		public NettyClientInitializer(NettyClientHandler handler, WebSocketClientParams params) {
-			this.handler = handler;
+		public NettyClientInitializer(WebSocketClientParams params) {
 			this.params = params;
 		}
 		@Override
 		protected void initChannel(SocketChannel ch) throws Exception {
 			ch.attr(ServerConstants.PROTOCOL_HEADER_ADAPTER).set(params.getProtocolHeaderAdapter());
 			ChannelPipeline pipeline = ch.pipeline();
-			pipeline.addLast(new HttpClientCodec(),
-					new HttpObjectAggregator(1024*1024*2),
-					WebSocketClientCompressionHandler.INSTANCE,
-					handler);
+			pipeline.addLast("HttpClientCodec", new HttpClientCodec());
+			pipeline.addLast("HttpObjectAggregator", new HttpObjectAggregator(1024*1024*2));
+			pipeline.addLast("NettyClientHandler", new NettyClientHandler(params));
 		}
 	}
 
 
-	private class NettyClientHandler extends SimpleChannelInboundHandler<Object> {
+	private class NettyClientHandler extends SimpleChannelInboundHandler<FullHttpResponse> {
 		private WebSocketClientHandshaker handshaker;
 		private ChannelPromise handshakeFuture;
-
+		private WebSocketClientParams params;
 		public ChannelFuture handshakeFuture() {
 			return handshakeFuture;
 		}
 
-		public NettyClientHandler(WebSocketClientHandshaker handshaker){
-			this.handshaker = handshaker;
+		public NettyClientHandler(WebSocketClientParams params){
+			this.params = params;
+
+			this.handshaker = WebSocketClientHandshakerFactory.newHandshaker(
+				params.getURI(), WebSocketVersion.V13, null, true, new DefaultHttpHeaders());
 		}
 
 
@@ -115,38 +107,37 @@ public class NettyWebsocketClient implements ILongConnClient {
 			handshaker.handshake(ctx.channel());
 		}
 
-
-
 		@Override
-		public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+		public void channelRead0(ChannelHandlerContext ctx, FullHttpResponse msg) throws Exception {
 			if (!handshaker.isHandshakeComplete()) {
 				try {
-					handshaker.finishHandshake(ctx.channel(), (FullHttpResponse) msg);
+					handshaker.finishHandshake(ctx.channel(), msg);
+
+					ChannelPipeline pipeline = ctx.channel().pipeline();
+					pipeline.remove(this);
+
+
+					pipeline.addLast("WebSocketFrameToByteBufHandler", new WebSocketFrameToByteBufHandler());
+					pipeline.addLast("WebSocketDecoder", new WebSocketDecoder(params.getMaxReceivedLength(), params.isEncryption()));
+					pipeline.addLast("WebSocketServerHandler", new NettyWSClientHandler());
+					pipeline.addLast("encode", new WebSocketEncoder());
+
 					handshakeFuture.setSuccess();
 				} catch (WebSocketHandshakeException e) {
 					System.out.println("WebSocket Client failed to connect");
 					handshakeFuture.setFailure(e);
 				}
-				return;
+				ctx.fireChannelRead(msg.retain());
 			}
-
-			if (msg instanceof FullHttpResponse) {
-				throw new IllegalStateException("Unexpected FullHttpResponse (getStatus=" + ((FullHttpResponse) msg).status());
-			}
-
-			if (! (msg instanceof BinaryWebSocketFrame)) {
-				System.out.println("Not a Binary WebSocket Frame");
-				return;
-			}
-
-			BinaryWebSocketFrame webSocketFrame = ((BinaryWebSocketFrame) msg);
-			IProtocolHeader header = ChannelUtil.getProtolHeaderAdapter(ctx.channel()).newHeader(webSocketFrame.content());
-			byte [] bytes = new byte[webSocketFrame.content().readableBytes()];
-			webSocketFrame.content().readBytes(bytes);
-			trigger.response(new MessageContent(header.getProtocolId() ,bytes));
 		}
 	}
 
+	private class NettyWSClientHandler extends SimpleChannelInboundHandler<MessageContent> {
+		@Override
+		protected void channelRead0(ChannelHandlerContext ctx, MessageContent msg) throws Exception {
+			trigger.response(msg);
+		}
+	}
 	public static void shutdown(){
 		if (! group.isShutdown()) group.shutdownGracefully();
 	}
