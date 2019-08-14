d4c3d39a5f1da613df621b9e49f0c149a558d01d
qiunet <cLAsj8.mU2cgJ&P>
2019-08-14 15:50:05 +0800

    先给出entity的xsd 和 xml

---
+++ ProjectInit/src/test/java/org/qiunet/project/init/redis/RedisDataUtil.java
@@ -0,0 +1,29 @@
+package org.qiunet.project.init.redis;
+
+import org.qiunet.data.core.support.redis.BasePoolRedisUtil;
+import org.qiunet.data.core.support.redis.IRedisCaller;
+import org.qiunet.data.util.DbProperties;
+import redis.clients.jedis.JedisCommands;
+
+public class RedisDataUtil extends BasePoolRedisUtil {
+	private static final RedisDataUtil instance = new RedisDataUtil();
+	private RedisDataUtil() {
+		super(DbProperties.getInstance(), "data");
+	}
+
+	public static RedisDataUtil getInstance() {
+		return instance;
+	}
+
+	public static JedisCommands jedis(){
+		return instance.returnJedis();
+	}
+
+	public static JedisCommands jedis(boolean log){
+		return instance.returnJedis(log);
+	}
+
+	public static <T> T executorCommands(IRedisCaller<T> caller) {
+		return instance.execCommands(caller);
+	}
+}
