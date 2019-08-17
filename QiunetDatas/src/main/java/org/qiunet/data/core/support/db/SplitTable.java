ea2fc81b045469b60ede57fabf056668a7aec9a5
qiunet <qiunet@gmail.com>
2019-08-17 08:48:07 +0800

    定义给Entity2Table 区分分表的注解

---
+++ QiunetDatas/src/main/java/org/qiunet/data/core/support/db/SplitTable.java
@@ -0,0 +1,18 @@
+package org.qiunet.data.core.support.db;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/****
+ * 需要分表的 {@link org.qiunet.data.core.entity.IEntityList}
+ * 加上这个注解
+ *
+ * 主要是给自动建表判断用的。
+ * 实际上list的sql语句加入 {tbIndex} 参数就行
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SplitTable {
+}
