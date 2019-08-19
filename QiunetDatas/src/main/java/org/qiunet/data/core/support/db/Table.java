4ad85f94365f517994c7a13cfde595a1800d9a24
qiunet <cLAsj8.mU2cgJ&P>
2019-08-19 11:48:54 +0800

    Table 注解

--- QiunetDatas/src/main/java/org/qiunet/data/core/support/db/Table.java
+++ QiunetDatas/src/main/java/org/qiunet/data/core/support/db/Table.java
@@ -7,12 +7,17 @@ import java.lang.annotation.Target;
 
 /****
  * 需要分表的 {@link org.qiunet.data.core.entity.IEntityList}
- * 加上这个注解
+ * 加上这个注解 {@link Table#splitTable()} = true
  *
  * 主要是给自动建表判断用的。
  * 实际上list的sql语句加入 {tbIndex} 参数就行
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface SplitTable {
+public @interface Table {
+	/**
+	 * 是否需要分表
+	 * @return
+	 */
+	boolean splitTable() default false;
 }
