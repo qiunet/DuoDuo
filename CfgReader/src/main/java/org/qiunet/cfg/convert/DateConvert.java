58490bc11860c588287b25a3fe7b91fa82822d3f
qiunet <qiunet@gmail.com>
2020-02-05 14:01:37 +0800

    feat(CfgReader): 增加DateConvert
    可以直接使用Date格式了 但是必须是 yyyy-MM-dd HH:mm:ss 格式

---
+++ CfgReader/src/main/java/org/qiunet/cfg/convert/DateConvert.java
@@ -0,0 +1,32 @@
+package org.qiunet.cfg.convert;
+
+import org.qiunet.utils.date.DateUtil;
+import org.qiunet.utils.logger.LoggerType;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/***
+ * 仅仅转换 "yyyy-MM-dd HH:mm:ss" 格式的字符串
+ * @author qiunet
+ * 2020-02-05 13:55
+ **/
+public class DateConvert extends BaseObjConvert<Date> {
+	private static SimpleDateFormat sdf = new SimpleDateFormat(DateUtil.DEFAULT_DATE_TIME_FORMAT);
+
+	@Override
+	protected Date fromString0(String str) {
+		try {
+			return sdf.parse(str);
+		} catch (ParseException e) {
+			LoggerType.DUODUO.error("Exception:", e);
+		}
+		return null;
+	}
+
+	@Override
+	public boolean canConvert(Class type) {
+		return Date.class == type;
+	}
+}
