7dfd9af9ab98b057467d9543ce697b4e2b909cf5
qiunet <cLAsj8.mU2cgJ&P>
2020-03-04 10:24:49 +0800

    feat(QiunetData): 停服时候, 执行一次更新数据.

--- QiunetDatas/src/main/java/org/qiunet/data/async/AsyncJobSupport.java
+++ QiunetDatas/src/main/java/org/qiunet/data/async/AsyncJobSupport.java
@@ -2,6 +2,9 @@ package org.qiunet.data.async;
 
 import org.qiunet.utils.asyncQuene.factory.DefaultThreadFactory;
 import org.qiunet.utils.hook.ShutdownHookThread;
+import org.qiunet.utils.listener.EventHandlerWeight;
+import org.qiunet.utils.listener.EventHandlerWeightType;
+import org.qiunet.utils.listener.data.ServerShutdownEventData;
 import org.qiunet.utils.logger.LoggerType;
 import org.qiunet.utils.math.MathUtil;
 import org.slf4j.Logger;
@@ -16,7 +19,7 @@ import java.util.concurrent.TimeUnit;
  * @author qiunet
  * Created on 17/2/11 08:04.
  */
- public class AsyncJobSupport {
+ public class AsyncJobSupport implements ServerShutdownEventData.ServerShutdownListener {
 	private Logger logger = LoggerType.DUODUO.getLogger();
 	private ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(
 			8,
@@ -49,5 +52,11 @@ import java.util.concurrent.TimeUnit;
 			}
 			// 会延迟一定时间执行 免得凑一块了.
 		}, MathUtil.random(0, (int) unit.toMillis(maxDelay)), TimeUnit.MILLISECONDS));
+	}
+
+	@Override
+	@EventHandlerWeight(EventHandlerWeightType.HIGHEST)
+	public void onShutdown(ServerShutdownEventData data) {
+		this.asyncToDb(100, TimeUnit.MILLISECONDS);
 	}
 }
