a8a92266b098d4466b9afe555d305aa48d8d6bc3
qiunet <cLAsj8.mU2cgJ&P>
2020-04-01 17:43:24 +0800

    feat(QiunetUtils): 位运算工具类

--- QiunetUtils/src/test/java/org/qiunet/utils/math/TestBitUtil.java
+++ QiunetUtils/src/test/java/org/qiunet/utils/math/TestBitUtil.java
@@ -12,10 +12,10 @@ import org.junit.Test;
 public class TestBitUtil {
 	@Test
 	public void testBit(){
-		int ss1 = BitUtil.writeBit(0, 12, 0);
+		int ss1 = BitUtil.writeBit(0, 13, 0); // 13的二进制 1101
 		int ss2 = BitUtil.writeBit(ss1, 142342, 4);
 
-		Assert.assertEquals(12, BitUtil.readBit(ss2, 0, 4));
+		Assert.assertEquals(13, BitUtil.readBit(ss2, 0, 4));
 		Assert.assertEquals(142342, BitUtil.readBit(ss2, 4, 28));
 	}
 }
