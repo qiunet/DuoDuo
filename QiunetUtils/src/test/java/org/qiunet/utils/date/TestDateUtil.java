3e09e256ec197bb48804451cec3f3f29cc1f34d4
zhengj <zheng0324jian@126.com>
2020-06-01 14:00:13 +0800

    同步date工具函数和测试

--- QiunetUtils/src/test/java/org/qiunet/utils/date/TestDateUtil.java
+++ QiunetUtils/src/test/java/org/qiunet/utils/date/TestDateUtil.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import java.text.ParseException;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.temporal.ChronoField;
 import java.util.Date;
 import java.util.concurrent.CountDownLatch;
@@ -75,5 +77,42 @@ public class TestDateUtil  extends BaseTest{
 
 		Assert.assertTrue(DateUtil.isSameDay(date1, date2));
 		Assert.assertTrue(DateUtil.isSameDay(date1, date3));
+	}
+
+	@Test
+	public void testDateTime() {
+		LocalDateTime nowLocalDateTime = DateUtil.nowLocalDateTime();
+		LocalDateTime nowLocalDateTimeUTC = DateUtil.nowLocalDateTime(ZoneOffset.UTC);
+
+		long milliByTime = DateUtil.getMilliByTime(nowLocalDateTime);
+		long milliByTimeUTC = DateUtil.getMilliByTime(nowLocalDateTimeUTC, ZoneOffset.UTC);
+
+		// 两次调用可能有个1毫秒时间的跨度.
+		Assert.assertTrue(Math.abs(milliByTime - milliByTimeUTC) < 2);
+
+		LocalDateTime localDateTime = DateUtil.stringToDate(DateUtil.dateToString(nowLocalDateTime));
+		LocalDateTime localDateTimeUTC = DateUtil.stringToDate(DateUtil.dateToString(nowLocalDateTimeUTC));
+
+		long milliByTime1 = DateUtil.getMilliByTime(localDateTime);
+		long milliByTime2 = DateUtil.getMilliByTime(localDateTimeUTC, ZoneOffset.UTC);
+
+		Assert.assertEquals(milliByTime1, milliByTime2);
+	}
+
+	@Test
+	public void testMilliToDateTime() {
+		long nowMilliByTime = DateUtil.getNowMilliByTime();
+//		System.out.println("当前时间戳:\t" + nowMilliByTime);
+
+		LocalDateTime localDateTime = DateUtil.getLocalDateTime(nowMilliByTime);
+		LocalDateTime localDateTimeUTC = DateUtil.getLocalDateTime(nowMilliByTime, ZoneOffset.UTC);
+
+		String dateStr1 = DateUtil.dateToString(localDateTime);
+		String dateStr2 = DateUtil.dateToString(localDateTimeUTC);
+		String dateStr3 = DateUtil.dateToString(DateUtil.addHours(localDateTimeUTC, 8));
+
+		Assert.assertNotEquals(dateStr1, dateStr2);
+		//+8 hour
+		Assert.assertEquals(dateStr1, dateStr3);
 	}
 }
