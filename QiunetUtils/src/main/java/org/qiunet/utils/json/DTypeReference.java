00b4ab0f94c28979fdff93b6434ac6b68e0512a8
qiunet <cLAsj8.mU2cgJ&P>
2020-10-16 15:24:27 +0800

    refactor(QiunetUtils): 优化JsonUtil

--- QiunetUtils/src/main/java/org/qiunet/utils/json/DTypeReference.java
+++ QiunetUtils/src/main/java/org/qiunet/utils/json/DTypeReference.java
@@ -9,7 +9,7 @@ import com.alibaba.fastjson.TypeReference;
  * @author qiunet
  * 2020-10-16 15:17
  */
-public class DTypeReference<T> extends TypeReference<T> {
+public final class DTypeReference<T> extends TypeReference<T> {
 	public DTypeReference(){
 		super();
 	}
--- QiunetUtils/src/main/java/org/qiunet/utils/json/TypeReferences.java
+++ QiunetUtils/src/main/java/org/qiunet/utils/json/TypeReferences.java
@@ -1,5 +1,7 @@
 package org.qiunet.utils.json;
 
+import com.alibaba.fastjson.TypeReference;
+
 import java.util.Map;
 import java.util.Set;
 
@@ -10,13 +12,13 @@ import java.util.Set;
  * 2020-09-18 11:52
  */
 public interface TypeReferences {
-	DTypeReference<Set<Long>> LONG_SET = new DTypeReference<>();
-	DTypeReference<Set<Integer>> INT_SET = new DTypeReference<>();
-	DTypeReference<Set<String>> STRING_SET = new DTypeReference<>();
-	DTypeReference<Map<Integer, Integer>> INT_INT_MAP = new DTypeReference<>();
-	DTypeReference<Map<String, String>> STRING_STRING_MAP = new DTypeReference<>();
-	DTypeReference<Map<Integer, String>> INT_STRING_MAP = new DTypeReference<>();
-	DTypeReference<Map<String, Integer>> STRING_INT_MAP = new DTypeReference<>();
-	DTypeReference<Map<Long, String>> LONG_STRING_MAP = new DTypeReference<>();
-	DTypeReference<Map<String, Long>> STRING_LONG_MAP = new DTypeReference<>();
+	TypeReference<Set<Long>> LONG_SET = new DTypeReference<>();
+	TypeReference<Set<Integer>> INT_SET = new DTypeReference<>();
+	TypeReference<Set<String>> STRING_SET = new DTypeReference<>();
+	TypeReference<Map<Integer, Integer>> INT_INT_MAP = new DTypeReference<>();
+	TypeReference<Map<String, String>> STRING_STRING_MAP = new DTypeReference<>();
+	TypeReference<Map<Integer, String>> INT_STRING_MAP = new DTypeReference<>();
+	TypeReference<Map<String, Integer>> STRING_INT_MAP = new DTypeReference<>();
+	TypeReference<Map<Long, String>> LONG_STRING_MAP = new DTypeReference<>();
+	TypeReference<Map<String, Long>> STRING_LONG_MAP = new DTypeReference<>();
 }
