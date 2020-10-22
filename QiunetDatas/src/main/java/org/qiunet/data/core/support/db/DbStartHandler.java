4b973e2de1b6084ff6daa57c9d72db7d89257cb5
qiunet <cLAsj8.mU2cgJ&P>
2020-10-22 10:12:03 +0800

    feat (QiunetDatas): 测试时候. 如果有依赖QiunetDatas. 某些情况不需要启动db loader

--- QiunetDatas/src/main/java/org/qiunet/data/core/support/db/DbStartHandler.java
+++ QiunetDatas/src/main/java/org/qiunet/data/core/support/db/DbStartHandler.java
@@ -1,6 +1,7 @@
 package org.qiunet.data.core.support.db;
 
 import org.qiunet.data.core.support.db.event.DbLoaderOverEventData;
+import org.qiunet.data.util.DbProperties;
 import org.qiunet.listener.event.EventHandlerWeightType;
 import org.qiunet.listener.event.EventListener;
 import org.qiunet.listener.event.data.ServerStartupEventData;
@@ -9,9 +10,15 @@ import org.qiunet.listener.event.data.ServerStartupEventData;
  * db 启动监听处理
  */
 class DbStartHandler {
+	/** 不少测试的地方. 不需要启动加载数据库. 但是有自动依赖. 使用该参数自动跳过. */
+	private static final String SKIP_TEST_START_LOADER = "skipTestLoader";
 
 	@EventListener(EventHandlerWeightType.HIGHEST)
 	public void onServerStartUp(ServerStartupEventData data) {
+		if (DbProperties.getInstance().getBoolean(SKIP_TEST_START_LOADER)) {
+			return;
+		}
+
 		DbLoader.getInstance();
 		DbLoaderOverEventData.fireEvent();
 	}
