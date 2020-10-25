4d5a01957a0776581be35d1bb3e87c31e4ac257c
qiunet <qiunet@gmail.com>
2020-10-25 13:58:39 +0800

    feat(QiunetUtils): 增加intIdGenerator

---
+++ QiunetUtils/src/main/java/org/qiunet/utils/id/IntIdGenerator.java
@@ -0,0 +1,25 @@
+package org.qiunet.utils.id;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/***
+ *
+ * @author qiunet
+ * 2020-06-27 21:32
+ **/
+public class IntIdGenerator {
+
+	private AtomicInteger generator;
+
+	public IntIdGenerator() {
+		this(0);
+	}
+
+	public IntIdGenerator(int initVal) {
+		this.generator = new AtomicInteger(initVal);
+	}
+
+	public int makeId() {
+		return this.generator.incrementAndGet();
+	}
+}
