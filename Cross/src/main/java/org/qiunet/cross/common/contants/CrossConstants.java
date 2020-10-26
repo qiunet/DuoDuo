e8379370055e35930d0e7daec34ffd2b8b351772
qiunet <cLAsj8.mU2cgJ&P>
2020-10-26 12:53:32 +0800

    refactor(Cross): 优化constants

--- Cross/src/main/java/org/qiunet/cross/common/contants/CrossConstants.java
+++ Cross/src/main/java/org/qiunet/cross/common/contants/CrossConstants.java
@@ -1,9 +1,10 @@
 package org.qiunet.cross.common.contants;
 
+import org.qiunet.cross.actor.CrossPlayerActor;
 import org.qiunet.cross.common.config.ServerConfig;
-import org.qiunet.cross.common.start.DefaultCommunicationStartUpContext;
-import org.qiunet.cross.common.start.DefaultCrossStartUpContext;
+import org.qiunet.cross.node.ServerNode;
 import org.qiunet.flash.handler.netty.server.param.TcpBootstrapParams;
+import org.qiunet.flash.handler.netty.server.param.adapter.IStartupContext;
 
 /***
  *
@@ -11,19 +12,27 @@ import org.qiunet.flash.handler.netty.server.param.TcpBootstrapParams;
  * @author qiunet
  * 2020-10-21 18:28
  */
-public class CrossConstants {
+public final class CrossConstants {
+	/**
+	 * 默认的cross 启动上下文
+	 */
+	public static final IStartupContext<CrossPlayerActor> DEFAULT_CROSS_START_CONTEXT = CrossPlayerActor::new;
+	/**
+	 * 默认的cross server node 启动上下文
+	 */
+	public static final IStartupContext<ServerNode> DEFAULT_CROSS_NODE_START_CONTEXT = ServerNode::new;
 	/**
 	 * cross 服务 tcp 默认启动参数
 	 */
 	public static final TcpBootstrapParams CROSS_SERVER_TCP_BOOTSTRAP_PARAMS = TcpBootstrapParams.custom()
-		.setStartupContext(new DefaultCrossStartUpContext())
+		.setStartupContext(DEFAULT_CROSS_START_CONTEXT)
 		.setPort(ServerConfig.getServerPort())
 		.build();
 	/**
 	 * 服务与服务之间通讯的启动参数
 	 */
 	public static final TcpBootstrapParams COMMUNICATION_SERVER_TCP_BOOTSTRAP_PARAMS = TcpBootstrapParams.custom()
-		.setStartupContext(new DefaultCommunicationStartUpContext())
+		.setStartupContext(DEFAULT_CROSS_NODE_START_CONTEXT)
 		.setPort(ServerConfig.getCommunicationPort())
 		.build();
 }
