821c7c885c68ee45b55ea40b4e88ac31c93466d2
qiunet <qiunet@gmail.com>
2020-10-29 22:56:08 +0800

    feat (QiunetData): ServerType 做成常量. 别的逻辑可能用到

--- QiunetDatas/src/main/java/org/qiunet/data/util/ServerConfig.java
+++ QiunetDatas/src/main/java/org/qiunet/data/util/ServerConfig.java
@@ -9,28 +9,34 @@ import org.qiunet.utils.properties.anno.DPropertiesValue;
  * @author qiunet
  * 2020-08-04 10:50
  ***/
-@DProperties("server.properties")
+@DProperties(ServerConfig.PROPERTIES_FILE_NAME)
 public class ServerConfig {
+	public static final String PROPERTIES_FILE_NAME = "server.properties";
+	public static final String HORT_PORT = "hook.port";
+	public static final String SERVER_ID = "server.id";
+	public static final String SERVER_TYPE = "server.type";
+	public static final String SERVER_PORT = "server.port";
+	public static final String COMMUNICATION_PORT = "communication.port";
 
-	@DPropertiesValue("server.id")
+	@DPropertiesValue(SERVER_ID)
 	private static int serverId;
 
-	@DPropertiesValue("server.type")
+	@DPropertiesValue(SERVER_TYPE)
 	private static ServerType serverType;
 	/**
 	 * hook 的端口
 	 */
-	@DPropertiesValue("hook.port")
+	@DPropertiesValue(HORT_PORT)
 	private static int hookPort;
 	/**
 	 * 对外服务的端口.
 	 */
-	@DPropertiesValue("server.port")
+	@DPropertiesValue(SERVER_PORT)
 	private static int serverPort;
 	/**
 	 * 服务之间通讯的端口.
 	 */
-	@DPropertiesValue("communication.port")
+	@DPropertiesValue(COMMUNICATION_PORT)
 	private static int communicationPort;
 
 	public static int getServerId() {
