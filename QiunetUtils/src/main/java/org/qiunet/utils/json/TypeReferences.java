e40fadd0876acf0d47144ee80a41086d9d9c4208
qiunet <qiunet@gmail.com>
2020-11-07 14:11:54 +0800

    refactor(QiunetUtils): DTypeReference不能用

--- QiunetUtils/src/main/java/org/qiunet/utils/json/TypeReferences.java
+++ QiunetUtils/src/main/java/org/qiunet/utils/json/TypeReferences.java
@@ -12,13 +12,13 @@ import java.util.Set;
  * 2020-09-18 11:52
  */
 public interface TypeReferences {
-	TypeReference<Set<Long>> LONG_SET = new DTypeReference<>();
-	TypeReference<Set<Integer>> INT_SET = new DTypeReference<>();
-	TypeReference<Set<String>> STRING_SET = new DTypeReference<>();
-	TypeReference<Map<Integer, Integer>> INT_INT_MAP = new DTypeReference<>();
-	TypeReference<Map<String, String>> STRING_STRING_MAP = new DTypeReference<>();
-	TypeReference<Map<Integer, String>> INT_STRING_MAP = new DTypeReference<>();
-	TypeReference<Map<String, Integer>> STRING_INT_MAP = new DTypeReference<>();
-	TypeReference<Map<Long, String>> LONG_STRING_MAP = new DTypeReference<>();
-	TypeReference<Map<String, Long>> STRING_LONG_MAP = new DTypeReference<>();
+	TypeReference<Set<Long>> LONG_SET = new TypeReference<Set<Long>>(){};
+	TypeReference<Set<Integer>> INT_SET = new TypeReference<Set<Integer>>(){};
+	TypeReference<Set<String>> STRING_SET = new TypeReference<Set<String>>(){};
+	TypeReference<Map<Integer, Integer>> INT_INT_MAP = new TypeReference<Map<Integer, Integer>>(){};
+	TypeReference<Map<String, String>> STRING_STRING_MAP = new TypeReference<Map<String, String>>(){};
+	TypeReference<Map<Integer, String>> INT_STRING_MAP = new TypeReference<Map<Integer, String>>(){};
+	TypeReference<Map<String, Integer>> STRING_INT_MAP = new TypeReference<Map<String, Integer>>(){};
+	TypeReference<Map<Long, String>> LONG_STRING_MAP = new TypeReference<Map<Long, String>>(){};
+	TypeReference<Map<String, Long>> STRING_LONG_MAP = new TypeReference<Map<String, Long>>(){};
 }
