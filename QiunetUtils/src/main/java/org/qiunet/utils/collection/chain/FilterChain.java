634ec23d8f60f2e6467e7d136c3c0e67a6081e57
邱阳 <qiunet@163.com>
2021-01-06 20:43:52 +0800

    feat(Function): 增加FilterChain

---
+++ QiunetUtils/src/main/java/org/qiunet/utils/collection/chain/FilterChain.java
@@ -0,0 +1,97 @@
+package org.qiunet.utils.collection.chain;
+
+import com.google.common.collect.Lists;
+import org.qiunet.utils.collection.enums.ForEachResult;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/***
+ * 过滤的链表
+ *
+ * @author qiunet
+ * 2021-01-06 20:31
+ */
+public class FilterChain<E> {
+	/**
+	 * 所有的过滤器
+	 */
+	private List<E> filters;
+
+	/**
+	 * 添加元素e
+	 * @param e
+	 */
+	public void add(E e) {
+		if (filters == null) {
+			synchronized (this) {
+				if (filters == null) {
+					filters = Lists.newCopyOnWriteArrayList();
+				}
+			}
+		}
+		filters.add(e);
+	}
+
+	/**
+	 * 删除指定元素e
+	 * @param e
+	 * @return
+	 */
+	public boolean remove(E e) {
+		if (filters == null) {
+			return true;
+		}
+
+		return filters.remove(e);
+	}
+
+	/**
+	 * 是否包含 元素e
+	 * @param e
+	 * @return
+	 */
+	public boolean contains(E e) {
+		return filters.contains(e);
+	}
+
+	/**
+	 * 清理链表
+	 */
+	public void clean() {
+		this.filters = null;
+	}
+
+	/**
+	 * 循环chain
+	 * @param consumer
+	 */
+	public void forEach(Function<E, ForEachResult> consumer) {
+		this.forEach(consumer, null);
+	}
+
+	/**
+	 * 循环chain
+	 * @param consumer
+	 * @param predicate
+	 */
+	public void forEach(Function<E, ForEachResult> consumer, Predicate<E> predicate) {
+		if (filters == null) {
+			return;
+		}
+
+		for (E filter : this.filters) {
+			if (predicate != null && !predicate.test(filter)) {
+				continue;
+			}
+
+			ForEachResult result = consumer.apply(filter);
+			if (result == ForEachResult.BREAK) {
+				break;
+			}
+		}
+	}
+}
+
