b424e88d16deed15243402e3eeb0fbecf14aa40a
邱阳 <qiunet@163.com>
2021-01-07 13:01:40 +0800

    feat(Function): result 增加缓存. 增加参数

--- Function/src/main/java/org/qiunet/function/consume/ConsumeResult.java
+++ Function/src/main/java/org/qiunet/function/consume/ConsumeResult.java
@@ -1,23 +1,49 @@
 package org.qiunet.function.consume;
 
+import com.google.common.collect.Maps;
+import org.qiunet.flash.handler.context.status.IGameStatus;
+
+import java.util.Map;
+
 /**
  * 消耗的结果
  */
 public class ConsumeResult {
-	public static final ConsumeResult SUCCESS = new ConsumeResult(true);
-	public static final ConsumeResult FAIL = new ConsumeResult(false);
+	public static final ConsumeResult SUCCESS = valueOf(IGameStatus.SUCCESS);
 
 	/**
 	 * 消耗是否成功
 	 */
-	private final boolean success;
+	private IGameStatus status;
+	/**
+	 * 其它参数
+	 */
+	private Object [] params;
 
-	private ConsumeResult(boolean success) {
-		this.success = success;
+
+	private static final Map<IGameStatus, ConsumeResult> cached = Maps.newConcurrentMap();
+	private ConsumeResult(IGameStatus status, Object... params) {
+		this.status = status;
+		this.params = params;
+	}
+
+	private static ConsumeResult valueOf(IGameStatus status, Object ... params) {
+		if (params == null || params.length == 0) {
+			return cached.computeIfAbsent(status, ConsumeResult::new);
+		}
+		return new ConsumeResult(status, params);
+	}
+
+	public <T extends IGameStatus> T getStatus() {
+		return (T) status;
+	}
+
+	public Object[] getParams() {
+		return params;
 	}
 
 	public boolean isSuccess(){
-		return success;
+		return status == IGameStatus.SUCCESS;
 	}
 
 	public boolean isFail(){
--- Function/src/main/java/org/qiunet/function/reward/RewardResult.java
+++ Function/src/main/java/org/qiunet/function/reward/RewardResult.java
@@ -1,23 +1,49 @@
 package org.qiunet.function.reward;
 
+import com.google.common.collect.Maps;
+import org.qiunet.flash.handler.context.status.IGameStatus;
+
+import java.util.Map;
+
 /**
  * 消耗的结果
  */
 public class RewardResult {
-	public static final RewardResult SUCCESS = new RewardResult(true);
-	public static final RewardResult FAIL = new RewardResult(false);
+	public static final RewardResult SUCCESS = valueOf(IGameStatus.SUCCESS);
 
 	/**
 	 * 消耗是否成功
 	 */
-	private final boolean success;
+	private IGameStatus status;
+	/**
+	 * 其它参数
+	 */
+	private Object [] params;
 
-	private RewardResult(boolean success) {
-		this.success = success;
+
+	private static final Map<IGameStatus, RewardResult> cached = Maps.newConcurrentMap();
+	private RewardResult(IGameStatus status, Object... params) {
+		this.status = status;
+		this.params = params;
+	}
+
+	private static RewardResult valueOf(IGameStatus status, Object ... params) {
+		if (params == null || params.length == 0) {
+			return cached.computeIfAbsent(status, RewardResult::new);
+		}
+		return new RewardResult(status, params);
+	}
+
+	public <T extends IGameStatus> T getStatus() {
+		return (T) status;
+	}
+
+	public Object[] getParams() {
+		return params;
 	}
 
 	public boolean isSuccess(){
-		return success;
+		return status == IGameStatus.SUCCESS;
 	}
 
 	public boolean isFail(){
