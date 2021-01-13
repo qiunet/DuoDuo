1d2d8ebe73a20520390c98a785f1e3c6c4f32e63
邱阳 <qiunet@163.com>
2021-01-13 10:25:51 +0800

    feat(QiunetUtils): pollChooser 优化.

--- QiunetUtils/src/main/java/org/qiunet/utils/math/DefaultPollChooserFactory.java
+++ QiunetUtils/src/main/java/org/qiunet/utils/math/DefaultPollChooserFactory.java
@@ -1,5 +1,9 @@
 package org.qiunet.utils.math;
 
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -10,39 +14,45 @@ public class DefaultPollChooserFactory implements PollChooserFactory {
 	public static final DefaultPollChooserFactory DEFAULT = new DefaultPollChooserFactory();
 	private DefaultPollChooserFactory(){}
 	@Override
-	public <Element> PollChooser<Element> newChooser(Element[] executors) {
-		if (MathUtil.isPowerOfTwo(executors.length)) {
-			return new PowerOfTwoPollChooser(executors);
+	public <Element> PollChooser<Element> newChooser(List<Element> executors) {
+		ArrayList<Element> list = null;
+		if (executors instanceof ArrayList) {
+			list = ((ArrayList<Element>) executors);
+		}else {
+			list = Lists.newArrayList(executors);
+		}
+		if (MathUtil.isPowerOfTwo(executors.size())) {
+			return new PowerOfTwoPollChooser(list);
 		} else {
-			return new GenericPollChooser(executors);
+			return new GenericPollChooser(list);
 		}
 	}
 
 	private static final class PowerOfTwoPollChooser<Element> implements PollChooser<Element> {
 		private final AtomicInteger idx = new AtomicInteger();
-		private final Element[] elements;
+		private final ArrayList<Element> elements;
 
-		PowerOfTwoPollChooser(Element[] elements) {
+		PowerOfTwoPollChooser(ArrayList<Element> elements) {
 			this.elements = elements;
 		}
 
 		@Override
 		public Element next() {
-			return elements[idx.getAndIncrement() & elements.length - 1];
+			return elements.get(idx.getAndIncrement() & elements.size() - 1);
 		}
 	}
 
 	private static final class GenericPollChooser<Element> implements PollChooser<Element> {
 		private final AtomicInteger idx = new AtomicInteger();
-		private final Element[] elements;
+		private final ArrayList<Element> elements;
 
-		GenericPollChooser(Element[] elements) {
+		GenericPollChooser(ArrayList<Element> elements) {
 			this.elements = elements;
 		}
 
 		@Override
 		public Element next() {
-			return elements[Math.abs(idx.getAndIncrement() % elements.length)];
+			return elements.get(Math.abs(idx.getAndIncrement() % elements.size()));
 		}
 	}
 }
--- QiunetUtils/src/main/java/org/qiunet/utils/math/PollChooserFactory.java
+++ QiunetUtils/src/main/java/org/qiunet/utils/math/PollChooserFactory.java
@@ -1,5 +1,10 @@
 package org.qiunet.utils.math;
 
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by qiunet.
  * 17/11/29
@@ -9,7 +14,14 @@ public interface PollChooserFactory {
 	/**
 	 * Returns a new {@link PollChooser}.
 	 */
-	<Element> PollChooser<Element> newChooser(Element[] executors);
+	default <Element> PollChooser<Element> newChooser(Element[] executors) {
+		return newChooser(Lists.newArrayList(executors));
+	}
+	/**
+	 * Returns a new {@link PollChooser}.
+	 * 使用list 构造的, list在外部增加元素. 里面也会增加.
+	 */
+	<Element> PollChooser<Element> newChooser(List<Element> executors);
 
 	/**
 	 * Chooses the next {@link PollChooser} to use.
