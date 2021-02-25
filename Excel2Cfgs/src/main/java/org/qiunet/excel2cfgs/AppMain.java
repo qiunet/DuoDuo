f747335b728c54d476b5fe2ab1beb21177390aa6
qiunet <qiunet@gmail.com>
2021-02-25 08:23:32 +0800

    feat(Excel2Cfgs): 增加系统托盘

--- Excel2Cfgs/src/main/java/org/qiunet/excel2cfgs/AppMain.java
+++ Excel2Cfgs/src/main/java/org/qiunet/excel2cfgs/AppMain.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -25,16 +27,53 @@ public enum AppMain {
 	private JPanel mainPanelCenter;
 	private JLabel titleLabel;
 
+	private TrayIcon trayIcon;// 托盘图标
+	private SystemTray systemTray;// 系统托盘
+
+
 	public static void main(String[] args) {
         AppMain.instance.init();
     }
 
+    private void handlerTray(JFrame frame) {
+		if (! SystemTray.isSupported()) {
+			frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			return;
+		}
+
+		frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+		systemTray = SystemTray.getSystemTray();// 获得系统托盘的实例
+		trayIcon = new TrayIcon(UiConstant.IMAGE_ICON);
+		try {
+			systemTray.add(trayIcon);// 设置托盘的图标
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+		//为图标设置鼠标监听器
+		trayIcon.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//双击托盘窗口再现
+				if(e.getClickCount() == 2 )
+					frame.setExtendedState(Frame.NORMAL);
+					frame.setVisible(true);
+				}
+		});
+
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowIconified(WindowEvent e) {
+				frame.dispose();
+			}
+		});
+	}
+
     private void init(){
 		JFrame frame = new JFrame(UiConstant.MAIN_WINDOWS_TITLE);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setBackground(UiConstant.MAIN_BACK_COLOR);
         frame.setIconImage(UiConstant.IMAGE_ICON);
         frame.setSize(UiConstant.MAIN_SIZE);
+        this.handlerTray(frame);
 
         JPanel mainPanel = this.createMainPanel();
         frame.addWindowListener(new WindowAdapter() {
