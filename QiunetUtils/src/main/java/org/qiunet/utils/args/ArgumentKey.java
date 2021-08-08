19ee4b6bfdfa6b9f0512a4c5341cc00dc13a0888
qiunet <qiunet@gmail.com>
2021-08-08 19:24:59 +0800

    feat(QiunetUtils): 在ArgumentKey 增加一些遍利的方法.

--- QiunetUtils/src/main/java/org/qiunet/utils/args/ArgumentKey.java
+++ QiunetUtils/src/main/java/org/qiunet/utils/args/ArgumentKey.java
@@ -11,4 +11,47 @@ public final class ArgumentKey<T> {
 	Argument<T> newAttribute() {
 		return new Argument<>(this);
 	}
+
+	/**
+	 * 从container get 到对应的值.
+	 * @param container 容器对象
+	 * @param <Container> 容器类
+	 * @return 对应的值
+	 */
+	public <Container extends IArgsContainer> T get(Container container) {
+		return container.getVal(this);
+	}
+
+	/**
+	 * 设置指定的值到容器.
+	 * @param container 容器对象
+	 * @param newVal 值
+	 * @param <Container> 容器类
+	 * @return 返回旧值
+	 */
+	public <Container extends IArgsContainer> T set(Container container, T newVal) {
+		return container.setVal(this, newVal);
+	}
+
+	/**
+	 * 容器中指定的key是否有值.
+	 * @param container 容器对象
+	 * @param <Container> 容器类
+	 * @return true 空
+	 */
+	public <Container extends IArgsContainer> boolean isEmpty(Container container){
+		return container.isEmpty(this);
+	}
+
+	/**
+	 * compare and set value
+	 * @param container 容器对象
+	 * @param oldVal 旧值
+	 * @param newVal 新值
+	 * @param <Container> 容器类
+	 * @return cas的结果 true 为成功.
+	 */
+	public <Container extends IArgsContainer> boolean cas(Container container, T oldVal, T newVal) {
+		return container.compareAndSet(this, oldVal, newVal);
+	}
 }
