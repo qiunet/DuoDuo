ceb05cebef26d4cf8d2aba7281f587a6c5c87abd
qiunet <qiunet@gmail.com>
2021-09-01 11:22:30 +0800

    feat(Function.ai, GameTest): 登录测试通过.

--- GameTest/src/test/java/org/qiunet/game/tests/client/action/login/LoginBehaviorBuilder.java
+++ GameTest/src/test/java/org/qiunet/game/tests/client/action/login/LoginBehaviorBuilder.java
@@ -2,7 +2,6 @@ package org.qiunet.game.tests.client.action.login;
 
 import org.qiunet.function.ai.builder.IBehaviorBuilder;
 import org.qiunet.function.ai.node.IBehaviorNode;
-import org.qiunet.function.ai.node.decorator.CounterNode;
 import org.qiunet.function.ai.node.executor.SelectorExecutor;
 import org.qiunet.function.ai.node.executor.SequenceExecutor;
 import org.qiunet.game.test.robot.Robot;
@@ -21,6 +20,6 @@ public class LoginBehaviorBuilder implements IBehaviorBuilder<Robot> {
 		SequenceExecutor sequence = new SequenceExecutor(() -> new AuthCondition().invert().verify(robot).isSuccess());
 		SequenceExecutor registerBehavior = new SequenceExecutor().addChild(new RandomNameAction(robot), new RegisterAction(robot), new PlayerIndexAction(robot));
 		sequence.addChild(new LoginAction(robot), new SelectorExecutor().addChild(registerBehavior, new PlayerIndexAction(robot)));
-		return new CounterNode(sequence, 1);
+		return sequence;
 	}
 }
