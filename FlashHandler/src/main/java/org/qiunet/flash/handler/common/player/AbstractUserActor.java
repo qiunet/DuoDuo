c953f1f2f2f7be31dfd3d786e0e51b2aa4e116cc
qiuxiangyang <qiuxiangyang@newlang.cn>
2021-09-29 10:55:56 +0800

    feat(FlashHandler): 触发退出事件

--- FlashHandler/src/main/java/org/qiunet/flash/handler/common/player/AbstractUserActor.java
+++ FlashHandler/src/main/java/org/qiunet/flash/handler/common/player/AbstractUserActor.java
@@ -1,6 +1,9 @@
 package org.qiunet.flash.handler.common.player;
 
+import org.qiunet.flash.handler.common.player.event.BaseUserEventData;
+import org.qiunet.flash.handler.common.player.event.PlayerLogoutEventData;
 import org.qiunet.flash.handler.context.session.DSession;
+import org.qiunet.listener.event.EventManager;
 
 /***
  * 玩家类型的messageActor 继承该类
@@ -13,11 +16,26 @@ public abstract class AbstractUserActor<T extends AbstractUserActor<T>> extends 
 	public AbstractUserActor(DSession session) {
 		super(session);
 	}
-
 	/**
 	 * 是否跨服状态
 	 *
 	 * @return
 	 */
 	public abstract boolean isCrossStatus();
+
+	@Override
+	protected void setSession(DSession session) {
+		this.session = session;
+		this.session.addCloseListener(cause -> this.fireEvent(new PlayerLogoutEventData<T>((T) this, cause)));
+	}
+
+	/**
+	 * 对事件的处理.
+	 * 跨服的提交跨服那边. 本服调用自己的
+	 * @param eventData
+	 */
+	public <D extends BaseUserEventData<T>> void fireEvent(D eventData){
+		eventData.setPlayer((T) this);
+		EventManager.fireEventHandler(eventData);
+	}
 }
