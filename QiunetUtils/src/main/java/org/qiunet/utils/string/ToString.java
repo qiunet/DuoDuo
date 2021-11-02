5dd94e4c118bd7bfe2995acb5eea04f8c51eb7af
qiunet <qiunet@163.com>
2021-11-02 15:49:11 +0800

    feat(QiunetUtils): toString 修改下. 避免老是调用不必要的反射.

--- QiunetUtils/src/main/java/org/qiunet/utils/string/ToString.java
+++ QiunetUtils/src/main/java/org/qiunet/utils/string/ToString.java
@@ -1,10 +1,11 @@
 package org.qiunet.utils.string;
 
-import org.apache.commons.lang3.builder.MultilineRecursiveToStringStyle;
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import org.apache.commons.lang3.ArraySorter;
+import org.qiunet.utils.logger.LoggerType;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.*;
 
 /***
  * 对对象进行toString
@@ -12,42 +13,125 @@ import java.lang.reflect.Field;
  * @author qiunet
  * 2021/10/21 15:52
  **/
-public class ToString {
-	private static final ToStringStyle TO_STRING_STYLE = new DToStringStyle();
+public final class ToString {
+
+	/**
+	 * The {@code null} text {@code '&lt;null&gt;'}.
+	 */
+	private static final String nullText = "<null>";
+	/**
+	 * The content start {@code '['}.
+	 */
+	private static final String contentStart = "[";
+
+	/**
+	 * The content end {@code ']'}.
+	 */
+	private static final String contentEnd = "]";
+
+	/**
+	 * The field name value separator {@code '='}.
+	 */
+	private static final String fieldNameValueSeparator = " = ";
+	/**
+	 * The field separator {@code ','}.
+	 */
+	private static final String fieldSeparator = ", ";
+
+	/**
+	 * The array start <code>'{'</code>.
+	 */
+	private static final String arrayStart = "{";
+
+	/**
+	 * The array separator {@code ','}.
+	 */
+	private static final String arraySeparator = ",";
+
+
+	/**
+	 * The array end {@code '}'}.
+	 */
+	private static final String arrayEnd = "}";
+
+	private final Object obj;
+	ToString(Object obj) {
+		this.obj = obj;
+	}
 
 	public static String toString(Object obj) {
-		return new DReflectionToStringBuilder(obj, TO_STRING_STYLE).toString();
+		return new ToString(obj).toString();
 	}
 
-	/***
-	 * ToStringBuilderStyle 自定义
-	 *
-	 * @author qiunet
-	 * 2021/10/21 16:23
-	 **/
-	private static class DToStringStyle extends MultilineRecursiveToStringStyle {
-
-		public DToStringStyle() {
-			super();
-			this.setUseShortClassName(true);
-			this.setUseIdentityHashCode(false);
-		}
-
-
-	}
-
-	private static class DReflectionToStringBuilder extends ReflectionToStringBuilder {
-
-		public DReflectionToStringBuilder(Object object, ToStringStyle style) {
-			super(object, style);
-		}
-
-		@Override
-		protected boolean accept(Field field) {
-			if (field.getDeclaringClass() == Enum.class) {
-				return false;
+	private void appendFields(StringJoiner joiner) {
+		final Field[] fields = ArraySorter.sort(obj.getClass().getDeclaredFields(), Comparator.comparing(Field::getName));
+		for (Field field : fields) {
+			if (Modifier.isStatic(field.getModifiers())
+			 || Modifier.isTransient(field.getModifiers())
+			) {
+				continue;
 			}
-			return super.accept(field);
+
+			try {
+				field.setAccessible(true);
+				final Object fieldValue = field.get(obj);
+				joiner.add(field.getName() + fieldNameValueSeparator + this.valToString(fieldValue));
+			} catch (IllegalAccessException e) {
+				LoggerType.DUODUO.error("Object {} field {} get value error! {}", obj.getClass(), field.getName(), e);
+			}
 		}
+	}
+
+	/**
+	 *
+	 * @param fieldValue
+	 * @return
+	 */
+	private String valToString(Object fieldValue) {
+		if (fieldValue == null) {
+			return nullText;
+		}
+
+		Class<?> aClass = fieldValue.getClass();
+		if (aClass == String.class || aClass == Integer.class
+		 || aClass == Long.class || aClass == Boolean.class
+		 || aClass == Short.class || aClass == Float.class
+		 || aClass == Byte.class || aClass == Double.class
+		) {
+			return String.valueOf(fieldValue);
+		}
+
+		if (aClass.isEnum()) {
+			return ((Enum<?>) fieldValue).name();
+		}
+
+		if (aClass.isAssignableFrom(Date.class)) {
+			return String.valueOf(((Date) fieldValue).getTime());
+		}
+
+		if (aClass.isAssignableFrom(Map.class)) {
+			StringJoiner joiner = new StringJoiner(arraySeparator, arrayStart, arrayEnd);
+			((Map<?, ?>) fieldValue).forEach((key, val) -> joiner.add(this.valToString(key) + fieldNameValueSeparator + this.valToString(val)));
+			return joiner.toString();
+		}
+
+		if (aClass.isAssignableFrom(Collection.class)) {
+			StringJoiner joiner = new StringJoiner(arraySeparator, arrayStart, arrayEnd);
+			((Collection<?>) fieldValue).forEach(val -> joiner.add(this.valToString(val)));
+			return joiner.toString();
+		}
+
+		return ToString.toString(fieldValue);
+	}
+
+
+	@Override
+	public String toString() {
+		if (Objects.isNull(obj)) {
+			return nullText;
+		}
+		StringJoiner joiner = new StringJoiner(fieldSeparator, obj.getClass().getSimpleName() + contentStart, contentEnd);
+		this.appendFields(joiner);
+		return joiner.toString();
 	}
 }
