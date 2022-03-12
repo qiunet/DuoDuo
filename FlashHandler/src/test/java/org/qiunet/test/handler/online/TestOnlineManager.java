fff1410791ea223e198af1f6fafd989732a3592b
qiunet <qiunet@163.com>
2022-03-13 07:41:03 +0800

    feat(FlashHandler): 测试gitee 同步

--- FlashHandler/src/test/java/org/qiunet/test/handler/online/TestOnlineManager.java
+++ FlashHandler/src/test/java/org/qiunet/test/handler/online/TestOnlineManager.java
@@ -5,11 +5,16 @@ import io.netty.channel.ChannelPromise;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.qiunet.flash.handler.common.player.PlayerActor;
+import org.qiunet.flash.handler.common.player.UserOnlineManager;
 import org.qiunet.flash.handler.context.session.DSession;
 import org.qiunet.flash.handler.netty.server.constants.ServerConstants;
 import org.qiunet.utils.async.factory.DefaultThreadFactory;
+import org.qiunet.utils.scanner.ClassScanner;
+import org.qiunet.utils.scanner.ScannerType;
 
 /***
  *
@@ -19,8 +24,21 @@ import org.qiunet.utils.async.factory.DefaultThreadFactory;
 public class TestOnlineManager {
 	private static final EventLoopGroup eventExecutors = new NioEventLoopGroup(1, new DefaultThreadFactory("test-tcp-boss-event-loop-"));
 
+	@BeforeAll
+	public static void init() {
+		ClassScanner.getInstance(ScannerType.EVENT).scanner();
+	}
+
 	@Test
-	public void testLogin() {
+	public void testOnlineCount() {
+		this.buildPlayerActor(10);
+		Assertions.assertEquals(1, UserOnlineManager.instance.onlineSize());
+		this.buildPlayerActor(11);
+		Assertions.assertEquals(2, UserOnlineManager.instance.onlineSize());
+	}
+
+	@Test
+	public void testLogout() {
 
 	}
 
