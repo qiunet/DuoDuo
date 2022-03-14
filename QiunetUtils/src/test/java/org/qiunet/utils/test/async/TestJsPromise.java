8623aace712153a62c0684c660f4aaf7dad89f2c
qiunet <qiunet@163.com>
2022-03-14 21:25:25 +0800

    feat(QiunetUtils): JsPromise 模仿JavaScript Promise Api 做的

---
+++ QiunetUtils/src/test/java/org/qiunet/utils/test/async/TestJsPromise.java
@@ -0,0 +1,48 @@
+package org.qiunet.utils.test.async;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.qiunet.utils.async.promise.JsPromise;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/***
+ *
+ * @author qiunet
+ * 2022/3/14 17:52
+ */
+public class TestJsPromise {
+
+	@Test
+	public void thenAccept() {
+		AtomicReference<String> result = new AtomicReference<>();
+		new JsPromise<>(() -> "world")
+				.then(word -> "hello " + word)
+				.accept(result::set);
+		Assertions.assertEquals(result.get(), "hello world");
+	}
+
+
+	@Test
+	public void thenException() {
+		AtomicReference<Throwable> result = new AtomicReference<>();
+		new JsPromise<>(() -> "123")
+				.then(num -> "hello " + num)
+				.then(Integer::parseInt)
+				.exception(result::set);
+
+		Assertions.assertNotNull(result.get());
+	}
+
+	@Test
+	public void thenExceptionally() {
+		AtomicReference<Integer> result = new AtomicReference<>();
+		new JsPromise<>(() -> "123")
+				.then(num -> "hello " + num)
+				.then(Integer::parseInt)
+				.exceptionally(ex -> 5)
+				.accept(result::set);
+
+		Assertions.assertEquals(5, result.get());
+	}
+}
