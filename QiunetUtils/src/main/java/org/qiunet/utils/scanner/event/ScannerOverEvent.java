0752929d2801e8102fa30775d57b47e3d3e0e95a
qiunet <qiunet@gmail.com>
2024-05-16 22:03:42 +0800

    feat(Quartz): quartz 时区设置

---
+++ QiunetUtils/src/main/java/org/qiunet/utils/scanner/event/ScannerOverEvent.java
@@ -0,0 +1,14 @@
+package org.qiunet.utils.scanner.event;
+
+
+import org.qiunet.utils.listener.event.IListenerEvent;
+
+/***
+ *
+ * @author qiunet
+ * 2024/4/26 10:44
+ ***/
+public enum ScannerOverEvent implements IListenerEvent {
+
+	instance
+}
